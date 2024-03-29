fb285e5d40ffff2582cc41acd67e1b538b9b811f
70985_Sudhanshu <sudhanshukulkarni98@gmail.com>
2024-03-29 17:12:06 +0530

    adduser and fetchUserById methods working successfully

--- src/main/java/com/example/userinfo/service/UserService.java
+++ src/main/java/com/example/userinfo/service/UserService.java
@@ -1,12 +1,18 @@
 package com.example.userinfo.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import com.example.userinfo.dto.UserDTO;
 import com.example.userinfo.entity.User;
 import com.example.userinfo.mapper.UserMapper;
 import com.example.userinfo.repo.UserRepo;
 
+@Service
 public class UserService {
 
 	@Autowired
@@ -15,6 +21,15 @@ public class UserService {
 	public UserDTO addUser(UserDTO userDTO) {
 		User savedUser = userRepo.save(UserMapper.INSTANCE.mapUserDTOToUser(userDTO));
 		return UserMapper.INSTANCE.mapUserToUserDTO(savedUser);
+	}
+
+	public ResponseEntity<UserDTO> fetchUserDetailsById(Integer userId) {
+		Optional<User> fetchedUser = userRepo.findById(userId);
+		if(fetchedUser.isPresent()) {
+			return new ResponseEntity<>(UserMapper.INSTANCE.mapUserToUserDTO(fetchedUser.get()), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+		
 	}
 	
 }
